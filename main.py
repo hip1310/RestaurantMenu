9f24e51d40b7f4a5c77658e820c9c9824da9e7a7
hip1310 <harshpatel131090@gmail.com>
2017-09-20 20:34:52 -0400

    Add routing to view, add, edit and delete menu item

--- main.py
+++ main.py
@@ -14,21 +14,46 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-# decorator that wraps the HelloWorld() function inside
-# the app.route() function so that HelloWorld() gets called
+# Method to view restaurant menu for one restaurant by using restaurant id
+
+# @ specifies decorator that wraps the viewRestaurantMenu() function inside
+# the app.route() function so that viewRestaurantMenu() gets called
 # whenever the specified routes are sent from the browser
-@app.route('/menuitems')
-def getMenuItems():
-	restaurants = session.query(Restaurant).all()
+@app.route('/restaurant/<int:restaurant_id>')
+def viewRestaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
 	output = ''
-	for j in restaurants:	
-		menuItems = session.query(MenuItem).filter_by(restaurant_id = j.id)
-		output += j.name + '</br>' + '</br>'
-		for i in menuItems:
-			output += '&nbsp' + '&nbsp' + '&nbsp'
-			output += i.name
-			output += '</br>' + '</br>'
-        return output
+	menuItems = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+	output += restaurant.name + '</br>' + '</br>'
+	for i in menuItems:
+		output += '&nbsp' + '&nbsp' + '&nbsp'
+		output += i.name
+		output += '</br>'
+		output += '&nbsp' + '&nbsp' + '&nbsp'
+		output += i.description
+		output += '</br>'
+		output += '&nbsp' + '&nbsp' + '&nbsp'
+		output += i.price
+		output += '</br>' + '</br>'
+	return output
+
+# Method to add new restaurant menu item
+# for one restaurant by using restaurant id
+@app.route('/restaurant/<int:restaurant_id>/new')
+def addNewMenuItem(restaurant_id):
+	return "page to create a new menu item"
+
+# Method to edit restaurant menu item using item id
+# for one restaurant by using restaurant id
+@app.route('/restaurant/<int:restaurant_id>/<int:item_id>/edit')
+def editMenuItem(restaurant_id, item_id):
+	return "page to edit a menu item"
+
+# Method to delete restaurant menu item using item id
+# for one restaurant by using restaurant id
+@app.route('/restaurant/<int:restaurant_id>/<int:item_id>/delete')
+def deleteMenuItem(restaurant_id, item_id):
+	return "page to delete a menu item"
 
 # The python file ran by python interpreter gets the by default name __main__
 # While for all other imported python files, __name__ variable is set to
